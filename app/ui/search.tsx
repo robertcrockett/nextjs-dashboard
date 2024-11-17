50df6d198e7b18a9543e645a5722ade2f094960d
robertcrockett <robertwcrockett@hotmail.com>
2024-11-17 18:00:40 -0500

    Adding handleSearch for searching invoices.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,29 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter} from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  /**
+   * Handles updates to the search input using the URLSearchParams, converting the search into a query parameter
+   *
+   * @param term - The term being searched on
+   */
   function handleSearch(term: string) {
-    console.log(term)
+    const params = new URLSearchParams(searchParams);
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    // Replace the current url with the current path and a complete query string
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -18,6 +37,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
