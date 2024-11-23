425515348f9bc795f75a9a9b27a2a43eb979b555
robertcrockett <robertwcrockett@hotmail.com>
2024-11-23 18:20:02 -0500

    Adding a UI error-handling boundary.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+// @ts-ignore
 import { expirePath } from "next/cache";
 import { redirect } from 'next/navigation';
 
@@ -25,10 +26,17 @@ export async function createInvoice(formData : FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+   `;
+  }
+  catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
 
   expirePath('/dashboard/invoices');
   redirect(`/dashboard/invoices`);
@@ -45,17 +53,32 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  }
+  catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.'
+    }
+  }
 
   expirePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  expirePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    expirePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  }
+  catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    }
+  }
 }
